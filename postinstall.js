bbde2481151985e65a47962a955ae6102e2956fb
hoangnecon <hoangnht.24ai@vku.udn.vn>
2025-11-04 19:24:20 +0700

    Fix postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,10 +1,7 @@
 const { execSync } = require('child_process');
 const path = require('path');
 
-<<<<<<< HEAD
-=======
 // Function to run a command in a specific directory
->>>>>>> 5fbd89e1940c5529e991b6f87c3bb3ecb6a1a785
 const runCommand = (command, directory) => {
   console.log(`\n--- Running command: ${command} in ${directory} ---\n`);
   try {
@@ -17,19 +14,11 @@ const runCommand = (command, directory) => {
 
 const main = () => {
   try {
-<<<<<<< HEAD
     // Install frontend dependencies, ignoring any scripts
     runCommand('npm install --ignore-scripts', path.join(__dirname, 'frontend'));
 
     // Install backend dependencies
     runCommand('python -m pip install -r backend/requirements.txt', path.join(__dirname, 'backend'));
-=======
-    // Install frontend dependencies
-    runCommand('npm install', path.join(__dirname, 'frontend'));
-
-    // Install backend dependencies
-    runCommand('python -m pip install -r requirements.txt', path.join(__dirname, 'backend'));
->>>>>>> 5fbd89e1940c5529e991b6f87c3bb3ecb6a1a785
 
     console.log('\n--- All dependencies installed successfully! ---\n');
   } catch (error) {
